3c238d69f75b810cdb48552d9f7549b9f57f90c7
JerryWSL <jerryshum2636@gmail.com>
2026-03-31 15:01:27 -0400

    setting camera defaults & environnment changes

--- src/experience/Experience.jsx
+++ src/experience/Experience.jsx
@@ -1,16 +1,26 @@
 import { Canvas } from '@react-three/fiber';
 import Scene from './Scene';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { useRef } from 'react';
 
 function Experience() {
    return (
       <>
-         <Canvas>
-            <OrbitControls />
+         <Canvas shadow={true}>
+            <color attach="background" args={['#171720']} />
+           <PerspectiveCamera
+  makeDefault
+  position={[4.398168475370966, 2.454488749802843, 3.2999620563677756]}
+  rotation={[-0.5289706589036337, 0.8415636501973147, 0.4110495587477473]}
+  fov={75}
+/>
+            <OrbitControls target={[0.27223779963070727, 0.2189944663811708, -0.5474290409918368]} />
             <Scene />
          </Canvas>
       </>
    );
 }
+
+
 
 export default Experience;
--- src/experience/Scene.jsx
+++ src/experience/Scene.jsx
@@ -1,10 +1,40 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import Model from './models/Real_portfolio';
+import { useThree } from '@react-three/fiber';
 
 function Scene() {
+   const { camera, controls } = useThree();
+
+   // useEffect(() => {
+   //    const handleKeyPress = (e) => {
+   //       if (e.key === 'c' || e.key === 'C') {
+   //          const position = camera.position.toArray();
+   //          const rotation = camera.rotation.toArray().slice(0, 3);
+   //          const fov = camera.fov;
+   //          const target = controls?.target.toArray() || [0, 0, 0];
+
+   //          console.log('--- CAMERA VIEW CONFIG ---');
+   //          console.log(
+   //             `<PerspectiveCamera\n  makeDefault\n  position={[${position.join(
+   //                ', '
+   //             )}]}\n  rotation={[${rotation.join(', ')}]}\n  fov={${fov}}\n/>`
+   //          );
+   //          console.log(`<OrbitControls target={[${target.join(', ')}]} />`);
+
+   //          console.log('---------------------------');
+   //       }
+   //    };
+
+   //    window.addEventListener('keydown', handleKeyPress);
+   //    return () => window.removeEventListener('keydown', handleKeyPress);
+   // }, [camera, controls]);
+
    return (
       <>
          <Suspense>
+            <fog attach="fog" args={['#171720', 5, 15]} />
+            <ambientLight intensity={1.5} />
+            <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
             <Model />
          </Suspense>
       </>
